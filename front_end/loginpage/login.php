27019720c244ff49c1bb245a89ce9f13e5d6ff9c
stonek4 <stonek4@rpi.edu>
2015-10-19 23:38:42 -0400

    Changed description, changed size of error text

--- front_end/loginpage/login.php
+++ front_end/loginpage/login.php
@@ -4,19 +4,23 @@ require_once(SITE_ROOT . '../PHP/User.php');
 require_once(SITE_ROOT . '../PHP/Course.php');
 
 session_start();
-if( isset($_SESSION) && isset($_SESSION['user']) ) {
+if( isset($_SESSION) && isset($_SESSION['user']) )
+{
 	$t = $_SESSION['user']->getIsAdmin();
-	if ($t == true) {
+	if ($t == true)
+	{
 		header( 'Location: ' . SITE_URL . '/front_end/Admin.php' ) ;
 	}
 
 	$t = $_SESSION['user']->getIsTA();
-	if ($t == true) {
+	if ($t == true)
+	{
 		header( 'Location: ' . SITE_URL . '/front_end/TA.php' ) ;
 	}
 
 	$t = $_SESSION['user']->getIsStudent();
-	if ($t == true) {
+	if ($t == true)
+	{
 		header( 'Location: ' . SITE_URL . '/front_end/student.php') ;
 	}
 }
@@ -28,7 +32,7 @@ if( isset($_SESSION) && isset($_SESSION['user']) ) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Inah Nail Art Salon is the place for beautiful nail art, massages, waxing, eyelash extensions, permanent makeup and more!">
+    <meta name="description" content="Taxiping Login Page">
 
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -69,7 +73,8 @@ if( isset($_SESSION) && isset($_SESSION['user']) ) {
 	<script src="http://code.jquery.com/jquery-1.11.1.js"></script>
 	<script type="text/javascript">
 		//move to seperate js later
-		$('.loginForm').submit(function (event) {
+		$('.loginForm').submit(function (event)
+		{
 			event.preventDefault();
 			console.log("logging in...");
 			var user = $('#email').val();
@@ -78,50 +83,55 @@ if( isset($_SESSION) && isset($_SESSION['user']) ) {
 			$('input').removeClass('error');
 			$('#login_error').empty();
 
-			if (!user || !pass) {
+			if (!user || !pass)
+			{
 				//highlight fields that aren't completed
-				if (!user) {
+				if (!user)
+				{
 					$('#email').addClass('error');
 				}
-				if (!pass) {
+				if (!pass)
+				{
 					$('#pass').addClass('error');
 				}
 				$('#login_error').html("Field(s) blank");
 				console.log("error");
 			}
-			else {
+			else
+			{
+				$.ajax(
+					{
+            url: "<?php echo SITE_URL; ?>/front_end/loginpage/validate_login.php",
+            data: { 'login': '1', 'user': user, 'pass': pass },
+            method: 'POST',
+            success: function (data)
+						{
+                data = $.trim(data);
+                console.log("data: " + data);
 
-				$.ajax({
-	                url: "<?php echo SITE_URL; ?>/front_end/loginpage/validate_login.php",
-	                data: { 'login': '1', 'user': user, 'pass': pass },
-	                method: 'POST',
-	                success: function (data) {
-	                    data = $.trim(data);
+                /*
+                  data is codes sent by validate_login.php
+                  1 is success
+                  0 is login error
+                */
 
-	                    console.log("data: " + data);
-
-	                    /*
-	                      data is codes sent by validate_login.php
-	                      1 is success
-	                      0 is login error
-	                    */
-
-	                    if (data === "0") {
-	                    	console.log("can't log in");
-	                        $('#login_error').html("User name and/or password invalid");
-	                    }
-						else {
-	                    	console.log("login successful")
-	                        document.location.href = '<?php echo SITE_URL; ?>/front_end/' + data;
-						}
-
-	                },
-	                error: function () {
-	                    console.log("error");
-	                }
-	            });
-
-			}
-		});
+                if (data === "0")
+								{
+                	console.log("can't log in");
+                  $('#login_error').html("User name and/or password invalid");
+                }
+								else
+								{
+			          	console.log("login successful")
+			            document.location.href = '<?php echo SITE_URL; ?>/front_end/' + data;
+								}
+            },
+            error: function ()
+						{
+                console.log("error");
+            }
+        	});
+				}
+			});
 	</script>
 </html>
