4bd4a4aae5114d56513289c4e080ebacf19795fe
Alex Kelleher <alexpkelleher@gmail.com>
2015-11-23 14:47:20 -0500

    remove unused generateRandomString()

--- PHP/User.php
+++ PHP/User.php
@@ -414,16 +414,6 @@ class User {
 		return $token;
 	}
 
-	public function generateRandomString($length = 10) {
-    	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	    $charactersLength = strlen($characters);
-	    $randomString = '';
-	    for ($i = 0; $i < $length; $i++) {
-	        $randomString .= $characters[rand(0, $charactersLength - 1)];
-	    }
-	    return $randomString;
-	}
-
 	public static function reset_password_update($sec_key, $password ){
 		
 		$db = DB::getInstance();
